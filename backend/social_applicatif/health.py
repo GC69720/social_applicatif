5fb1a04cd12aaad7ba5fa1fe4967fa875b884a40
Gregory Catarelli <gregory.catarellipro@gmail.com>
2025-10-25 01:47:11 +0200

    correctif syntaxe dans backend/social_applicatif/health.py

--- backend/social_applicatif/health.py
+++ backend/social_applicatif/health.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def health(request):
-    return HttpResponse("ok, ça farte !", content_type="text/plain", status=200)
+    return HttpResponse("ok", content_type="text/plain", status=200)
